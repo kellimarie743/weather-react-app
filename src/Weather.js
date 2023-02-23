018f9bcfbeb0266e156d203d173b5160218af297
kellimarie743 <108831417+kellimarie743@users.noreply.github.com>
2023-02-22 17:55:19 -0800

    update API call and reformat weather data

--- src/Weather.js
+++ src/Weather.js
@@ -1,51 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import "./App.css";
 
 export default function Weather() {
-  let weatherInfo = {
-    city: "Los Angeles",
-    date: "Sunday June 22nd, 2022",
-    temp: 75,
-    humidity: 30,
-    wind: 10,
-    vibe: "sunny with some clouds",
-  };
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
 
-  return (
-    <div className="Weather">
-      <form className="search">
-        <input type="text" placeholder="name a city.." autocomplete="off" />
-        <input type="submit" value="search" className="btn btn-success" />
-        <button className="btn btn-primary">Here 📍</button>
-      </form>
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form className="search">
+          <input type="text" placeholder="name a city.." autoComplete="off" />
+          <input type="submit" value="search" className="btn btn-success" />
+          <button className="btn btn-primary">Here 📍</button>
+        </form>
+        <h1 className="cityName">Los Angeles</h1>
 
-      <h1 className="cityName">{weatherInfo.city}</h1>
-      <div className="date">{weatherInfo.date}</div>
-      <div className="daily-temp">
-        <span>{weatherInfo.temp}</span>
-        <span className="celsiusFahren"> °F </span>
+        <span className="date">Sunday June 22nd, 2022</span>
+        <span className="daily-temp"> {Math.round(temperature)}°C</span>
 
         <ul>
-          <li>Vibe: {weatherInfo.vibe}</li>
-          <li>Wind: {weatherInfo.wind}mph</li>
-          <li>Humidity: {weatherInfo.humidity}%</li>
+          <li>Vibe: sunny with some clouds</li>
+          <li>Wind: 10mph</li>
+          <li>Humidity: 30%</li>
         </ul>
       </div>
+    );
+  }
+  const apiKey = "a3aef3d88b497be8dac5495183af8944";
+  let city = "New York";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then((response) => {
+    handleResponse(response.data);
+    return "App Loading...";
+  });
 
-      <div className="credits">
-        Hosted on{" "}
-        <a href="https://app.netlify.com/teams/kellimarie743/sites">Netlify</a>{" "}
-        with Open Sourced on{" "}
-        <a
-          href="https://github.com/kellimarie743/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        by Kelli Anderson
-      </div>
-    </div>
-  );
+  <div className="credits">
+    Hosted on{" "}
+    <a href="https://app.netlify.com/teams/kellimarie743/sites">Netlify</a> with
+    Open Sourced on{" "}
+    <a
+      href="https://github.com/kellimarie743/weather-react-app"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Github
+    </a>{" "}
+    by Kelli Anderson
+  </div>;
 }
