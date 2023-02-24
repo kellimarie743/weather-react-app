44ad52d611982e64c420a4d33caf06d930a53107
kellimarie743 <108831417+kellimarie743@users.noreply.github.com>
2023-02-23 21:58:59 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          Hosted on{" "}
+          <a href="https://app.netlify.com/teams/kellimarie743/sites">
+            Netlify
+          </a>{" "}
+          with Open Sourced on{" "}
+          <a
+            href="https://github.com/kellimarie743/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          by Kelli Anderson
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -40,18 +40,4 @@ export default function Weather() {
     handleResponse(response.data);
     return "App Loading...";
   });
-
-  <div className="credits">
-    Hosted on{" "}
-    <a href="https://app.netlify.com/teams/kellimarie743/sites">Netlify</a> with
-    Open Sourced on{" "}
-    <a
-      href="https://github.com/kellimarie743/weather-react-app"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Github
-    </a>{" "}
-    by Kelli Anderson
-  </div>;
 }
