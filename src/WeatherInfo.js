3c168435b8a5b68be70353aaf96f5ad8ae9c91e3
kellimarie743 <108831417+kellimarie743@users.noreply.github.com>
2023-02-27 17:36:43 -0800

    changed wind to imperial standard of mph

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -28,7 +28,7 @@ export default function WeatherInfo(props) {
 
         <div className="col-6">
           <ul>
-            <li>Wind: {props.data.wind}km/h</li>
+            <li>Wind: {props.data.wind}mph</li>
             <li>Humidity: {props.data.humidity}%</li>
           </ul>
         </div>
